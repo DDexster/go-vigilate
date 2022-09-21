c3a8cedc11d7a1daddd2e227916aed3d781a7bc6
Dmitry Bondarenko <bondarenko.dmitry.85@gmail.com>
2022-09-21 16:32:37 +0300

    initial commit

---
+++ static/admin/js/pusher-service.js
@@ -0,0 +1,16 @@
+function initPusher(pusherKey) {
+  const pusher = new Pusher(pusherKey, {
+    authEndPoint: '/pusher/auth',
+    wsHost: 'localhost',
+    wsPort: 4001,
+    forceTLS: false,
+    enabledTransports: ['ws', 'wss'],
+    disabledTransports: []
+  })
+
+  const publicChannel = pusher.subscribe('public-channel');
+
+  publicChannel.bind("test-event", (data) => {
+    successAlert(data.message);
+  })
+}
